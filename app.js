1b09dad09c6e98d4612b5acd660713cdf3af3293
csm <csm009177@gamil.com>
2023-10-04 12:10:49 +0900

    make server

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("hellow");
+const http = require('http');
+http.createServer(function(request, response){
+  let writeHeadObject = { 'Content-Type': 'text/html' };
+  response.writeHead(200, writeHeadObject);
+  response.end("hello");
+}).listen(8080);
