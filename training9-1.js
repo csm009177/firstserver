4a0eb6e2671b8f9320fb5f6209cc1964a25ea3fd
csm <csm009177@gmail.com>
2023-10-12 23:27:50 +0900

    make server callback success

--- training9-1.js
+++ training9-1.js
@@ -1,12 +1,12 @@
 
 function server(callback) {
-  let req = {request:"calling"};
-  let res = {response:"answer"};
+  let req = "calling";
+  let res = "answer";
  return callback(req, res);
 };
 
-
 server(function(req,res){
-  console.log(req.request);
-  console.log(res.response);
+  console.log(req);
+  console.log(res);
 });
+
