5a8982f7c25fc064b448b0330fee07e261bb8878
csm <csm009177@gmail.com>
2023-10-16 18:17:36 +0900

    ready to home

--- app2.js
+++ app2.js
@@ -1,6 +1,9 @@
 const http = require('http');
 http.createServer(function(req, res){
+  let doc = require('index.html')
+
   let writeHeadObject = { 'Content-Type' : 'text/html' };
   res.writeHead(200, writeHeadObject);
-  res.end("hellow")
+  res.end(doc);
+    
 }).listen(2020);
