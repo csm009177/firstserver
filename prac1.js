ccde8dd3e3537a3ec2ade8cb642c24c7ee788b5e
csm <csm009177@gmail.com>
2023-10-17 08:54:16 +0900

    test text/plain

--- prac1.js
+++ prac1.js
@@ -1,7 +1,9 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-
+  res.writeHead(200, {'Content-Type' : 'text/plain'})
+  // res.write('launch')
+  res.end('launch')
 })
 
 server.listen(8080, (err) => {
