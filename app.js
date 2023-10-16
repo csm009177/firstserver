61d7abdd1943042182a212f1cc86755b37b6abd2
csm <csm009177@gmail.com>
2023-10-16 17:54:34 +0900

    basic app.js

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const http = require('http');
 http.createServer(function(request, response){
   let writeHeadObject = { 'Content-Type': 'text/html' };
   response.writeHead(200, writeHeadObject);
-  response.end("hello");
+  response.end('hello');
 }).listen(8080);
