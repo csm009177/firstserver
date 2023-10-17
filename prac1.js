e870b6d4b1cbfc54da609c59781d2ea19b64639b
csm <csm009177@gmail.com>
2023-10-17 09:24:35 +0900

    test res.end()

--- prac1.js
+++ prac1.js
@@ -2,8 +2,9 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type' : 'text/html'})
-  // res.write('launch')
+  res.write('launch')
   res.end('launch')
+  res.write('test')
 })
 
 server.listen(8080, (err) => {
